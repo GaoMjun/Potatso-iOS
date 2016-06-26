753df9318c7492701a608fc3351a2a3035840db0
GaoMjun <jun2675731483@gmail.com>
2016-06-27 00:00:59 +0800

    no message

--- Potatso/AppInitializer.swift
+++ Potatso/AppInitializer.swift
@@ -8,45 +8,45 @@
 
 import Foundation
 import ICSMainFramework
-import Appirater
-import Fabric
-import Crashlytics
+//import Appirater
+//import Fabric
+//import Crashlytics
 
 let appID = "1070901416"
 
 class AppInitializer: NSObject, AppLifeCycleProtocol {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-        configLogging()
-        Fabric.with([Answers.self, Crashlytics.self])
-        configAppirater()
+//        configLogging()
+//        Fabric.with([Answers.self, Crashlytics.self])
+//        configAppirater()
         return true
     }
 
-    func configAppirater() {
-        Appirater.setAppId(appID)
-        Appirater.setUsesUntilPrompt(100)
-        Appirater.setDaysUntilPrompt(0)
-        Appirater.setTimeBeforeReminding(100)
-        Appirater.setSignificantEventsUntilPrompt(200)
-        Appirater.setDebug(false)
-        Appirater.appLaunched(true)
-    }
+//    func configAppirater() {
+//        Appirater.setAppId(appID)
+//        Appirater.setUsesUntilPrompt(100)
+//        Appirater.setDaysUntilPrompt(0)
+//        Appirater.setTimeBeforeReminding(100)
+//        Appirater.setSignificantEventsUntilPrompt(200)
+//        Appirater.setDebug(false)
+//        Appirater.appLaunched(true)
+//    }
 
-    func configLogging() {
-        DDLog.addLogger(DDTTYLogger.sharedInstance()) // TTY = Xcode console
-        DDLog.addLogger(DDASLLogger.sharedInstance()) // ASL = Apple System Logs
-
-        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
-        fileLogger.rollingFrequency = 60*60*24*3  // 24 hours
-        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
-        DDLog.addLogger(fileLogger)
-        #if DEBUG
-            DDLog.setLevel(DDLogLevel.All, forClass: DDTTYLogger.self)
-            DDLog.setLevel(DDLogLevel.All, forClass: DDASLLogger.self)
-        #else
-
-        #endif
-    }
+//    func configLogging() {
+//        DDLog.addLogger(DDTTYLogger.sharedInstance()) // TTY = Xcode console
+//        DDLog.addLogger(DDASLLogger.sharedInstance()) // ASL = Apple System Logs
+//
+//        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
+//        fileLogger.rollingFrequency = 60*60*24*3  // 24 hours
+//        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+//        DDLog.addLogger(fileLogger)
+//        #if DEBUG
+//            DDLog.setLevel(DDLogLevel.All, forClass: DDTTYLogger.self)
+//            DDLog.setLevel(DDLogLevel.All, forClass: DDASLLogger.self)
+//        #else
+//
+//        #endif
+//    }
     
 }
